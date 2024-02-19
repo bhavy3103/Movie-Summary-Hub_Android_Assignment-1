715f72bd6da02519b1790d06d09ad72bd2a40bfb
Bhavy <bhavypdevani31@gmail.com>
2024-02-20 00:40:57 +0530

    add spinne and set items for it and show in home activity

--- app/src/main/java/com/example/register/HomeActivity.java
+++ app/src/main/java/com/example/register/HomeActivity.java
@@ -6,6 +6,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -23,13 +27,33 @@ import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Spinner sort_spinner;
     private RecyclerView recyclerView;
     private RequestQueue requestQueue;
     private List<Movie> movieList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        sort_spinner = findViewById(R.id.sort_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinnerlist, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        sort_spinner.setAdapter(adapter);
+
+        sort_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                String data = adapterView.getItemAtPosition(position).toString();
+                Toast.makeText(HomeActivity.this, data, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setHasFixedSize(true);
@@ -39,22 +63,18 @@ public class HomeActivity extends AppCompatActivity {
 
         movieList = new ArrayList<>();
         fetchMovies();
-
-
     }
 
     private void fetchMovies() {
-
         String url = "https://dummyapi.online/api/movies";
 
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
+                        Log.d("name", "This is working");
 
-                        Log.d("name","This is working");
-
-                        for (int i = 0 ; i < response.length() ; i++){
+                        for (int i = 0; i < response.length(); i++) {
                             try {
                                 JSONObject jsonObject = response.getJSONObject(i);
                                 String title = jsonObject.getString("movie");
@@ -62,16 +82,15 @@ public class HomeActivity extends AppCompatActivity {
                                 String poster = jsonObject.getString("image");
                                 Double rating = jsonObject.getDouble("rating");
 
-                                Movie movie = new Movie(title , poster , overview , rating);
+                                Movie movie = new Movie(title, poster, overview, rating);
                                 movieList.add(movie);
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
-
-                            MovieAdapter adapter = new MovieAdapter(HomeActivity.this , movieList);
-
-                            recyclerView.setAdapter(adapter);
                         }
+
+                        MovieAdapter adapter = new MovieAdapter(HomeActivity.this, movieList);
+                        recyclerView.setAdapter(adapter);
                     }
                 }, new Response.ErrorListener() {
             @Override
